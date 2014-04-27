5f8f3b7347e379deb9f18b1b9e2f1c1f8265b489
Robert Wettlaufer <rdwettlaufer@gmail.com>
2014-04-26 23:58:21 -0400

    first working commit

--- modules/passwordGenerator.js
+++ modules/passwordGenerator.js
@@ -2,22 +2,31 @@ var dictArray = [];
 var numWords  = process.argv[2] || 4;
 var fs        = require('fs');
 var _         = require('underscore');
-var ret = [];
 
 // http://stackoverflow.com/a/15494283/369706
-var lines = fs.readFileSync('./brit-a-z.txt', 'utf8').split('\n');
+var lines = fs.readFileSync('./modules/reduced-brit-a-z.txt', 'utf8').split('\n');
 for (var l in lines){
-  if (lines[l].length > 3 && lines[l].length < 10 && lines[l].indexOf('\'') === -1) {
-    var line = lines[l];
+  var line = lines[l];
+  if (line) {
     dictArray.push(line[0].toUpperCase() + line.slice(1));
   }
 }
 
 var max = dictArray.length;
-for (var i = 0; i < numWords; i++) {
-  var word = dictArray[_.random(0, max)];
-  ret.push(word.slice(0, -1));
+
+function buildPassword(num) {
+  num = num || 4;
+  var ret = [];
+  for (var i = 0; i < num; i++) {
+    var word = dictArray[_.random(0, max)];
+    ret.push(word.slice(0, -1));
+  }
+  return {
+    numWords: num,
+    pass: ret.join(" . ")
+  };
 }
 
-console.log("\n\nYour password comprised of "+ numWords +" random words, my good sir:\n\n" + ret.join("") + "\n");
-
+module.exports = {
+  getPass: buildPassword
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var passGen = require('../modules/passwordGenerator.js');
-
+var password = require('../modules/passwordGenerator.js');
 /*
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+var getPass = function (num) {
+    num = num || 4;
+    var _password = password(num);
+    return _password;
 };
+
+var index = function(req, res) {
+  var num = req.params.num || 4;
+  var pass = password.getPass(num);
+  res.render('index', {
+    pass: pass.pass,
+    topTitle: 'CHBS',
+    title: 'CorrectHorseBatteryStaple',
+    subHeader: "getcho'self a new password playah"
+  });
+};
+
+exports.index = index;
