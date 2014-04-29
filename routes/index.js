085c24b5276e3fadf7df98748cd86a3932f1f9c9
Rob Wettlaufer <rdwettlaufer@gmail.com>
2014-04-29 16:31:52 -0400

    housekeeping, would you like new towel?

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,24 @@
 var password = require('../modules/passwordGenerator.js');
-/*
- * GET home page.
- */
 
-var getPass = function (num) {
-    num = num || 4;
-    var _password = password(num);
-    return _password;
-};
 
-var index = function(req, res) {
-  var num = req.params.num || 4;
-  var pass = password.getPass(num);
-  res.render('index', {
-    pass: pass.pass,
-    topTitle: 'CHBS',
-    title: 'Correct Horse Battery Staple',
-    subHeader: "click to get a new password"
-  });
-};
+module.exports = {
+  index: function(req, res) {
+    var num = req.params.num || 4;
+    var pass = password.getPass(num);
+    res.render('index', {
+      pass: pass.pass,
+      topTitle: 'CHBS',
+      title: 'Correct Horse Battery Staple',
+      subHeader: "click to get a new password"
+    });
+  },
 
-var api = function(req, res) {
-  var num = req.params.num || 4;
-  var pass = password.getPass(num, true);
-  res.writeHead(200, {'Content-Type': 'text/json' });
-  res.write(pass.pass);
-  res.end();
+
+  api: function(req, res) {
+    var num = req.params.num || 4;
+    var pass = password.getPass(num, true);
+    res.writeHead(200, {'Content-Type': 'text/json' });
+    res.write(pass.pass);
+    res.end();
+  }
 }
-
-exports.index = index;
-exports.api   = api;
