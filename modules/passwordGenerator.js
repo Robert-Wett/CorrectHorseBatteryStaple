c91fcaac59b5176aaccfb04044e750574e19dd69
Rob Wettlaufer <rdwettlaufer@gmail.com>
2014-04-29 16:28:47 -0400

    housekeeping, would you like new towel?

--- modules/passwordGenerator.js
+++ modules/passwordGenerator.js
@@ -1,19 +1,11 @@
 var dictArray = [];
-var numWords  = process.argv[2] || 4;
 var fs        = require('fs');
 var _         = require('underscore');
 
-// http://stackoverflow.com/a/15494283/369706
 var lines = fs.readFileSync('./modules/reduced-brit-a-z.txt', 'utf8').split('\n');
-for (var l in lines){
-  var line = lines[l];
-  if (line) {
-    dictArray.push(line);
-    // Instead of doing this stuff everytime, I've just altered the
-    // data source to include these changes
-    // dictArray.push(line[0].toUpperCase() + line.slice(1));
-  }
-}
+_.map(lines, function(line) {
+  dictArray.push(line);
+})
 
 var max = dictArray.length;
 
